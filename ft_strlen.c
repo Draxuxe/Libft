29f6b15f90e1d02466a21e402f3793eeed4ce510
Draxuxe <louis.filloux53@gmail.com>
2022-10-06 16:58:06 +0200

    Strlen secure

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: lfilloux <lfilloux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 13:50:16 by lfilloux          #+#    #+#             */
-/*   Updated: 2021/11/02 14:24:59 by lfilloux         ###   ########.fr       */
+/*   Updated: 2022/10/06 16:57:45 by lfilloux         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
+	if (!s)
+		return (0);
 	while (s[i])
 		i ++;
 	return (i);
